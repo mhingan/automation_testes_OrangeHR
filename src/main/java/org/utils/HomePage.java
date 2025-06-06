4a65dcbc9c871574d11ceb8422f0d54a03b47729
mhingan <mhingan6@gamil.com>
2025-06-06 11:10:13 +0300

    test LoginPage

--- src/main/java/org/utils/HomePage.java
+++ src/main/java/org/utils/HomePage.java
@@ -1,4 +1,21 @@
 package org.utils;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class HomePage {
+    private WebDriver driver;
+
+    private By upgr = By.xpath("//button[@class='oxd-glass-button orangehrm-upgrade-button']']");
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+
+
+    public String getBreadCrumbLandingVerif() {
+
+        return driver.findElement(upgr).getText();
+    }
 }
--- src/main/java/org/utils/ResetPasswordPage.java
+++ src/main/java/org/utils/ResetPasswordPage.java
@@ -1,4 +1,11 @@
 package org.utils;
 
+import org.openqa.selenium.WebDriver;
+
 public class ResetPasswordPage {
+    private WebDriver driver;
+
+    public ResetPasswordPage(WebDriver driver) {
+        this.driver = driver;
+    }
 }
